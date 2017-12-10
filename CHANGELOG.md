8fd4bbe8fa22b803f7473a3293235cc0c7cfb29a
Richard North <rich.north@gmail.com>
2017-12-10 16:52:55 +0000

    Add release credentials

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
 
-## UNRELEASED
+## 1.0.0 - 2017-12-10
 
 * Initial release, extracted from version 2.2.0 of [Visible Assertions](https://github.com/rnorth/visible-assertions)
--- release/deploy.sh
+++ release/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-openssl aes-256-cbc -K $encrypted_99aebaea6bf8_key -iv $encrypted_99aebaea6bf8_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
+openssl aes-256-cbc -K $encrypted_6444aba4c864_key -iv $encrypted_6444aba4c864_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
 gpg --fast-import release/codesigning.asc
 
 ./mvnw versions:set -DnewVersion=$TRAVIS_TAG
